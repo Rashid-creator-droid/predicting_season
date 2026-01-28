41156d5947fe0f4711b04dbdca0c8046a8613819
Rashid <valentaine1234@gmail.com>
2026-01-28 18:53:35 +0300

    Resnet18 model add

---
+++ ml/models/resnet.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+def build_model(num_classes: int = 4):
+    model = models.resnet18(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    return model
