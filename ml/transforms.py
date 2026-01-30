e9292108cf751f330cbb2435390bca923892f64f
Rashid <valentaine1234@gmail.com>
2026-01-30 04:07:02 +0300

    augmentation dataset

--- ml/transforms.py
+++ ml/transforms.py
@@ -1,19 +1,30 @@
 from torchvision import transforms
+from .config import IMAGENET_MEAN, IMAGENET_STD
 
 def get_train_transforms():
     return transforms.Compose([
-        transforms.Resize((256)),
-        # transforms.RandomRotation(15),
-        # transforms.CenterCrop(224),
-        # transforms.RandomHorizontalFlip(p=0.5),
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224),
+        transforms.RandomRotation(20),
+        transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
     ])
+
 
 def get_val_transforms():
     return transforms.Compose([
-        transforms.Resize((256)),
+        transforms.Resize((256, 256)),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+    ])
+
+
+def get_pred_transforms():
+    return transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
     ])
