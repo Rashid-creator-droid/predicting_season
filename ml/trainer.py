7e98f355af541b8121f60c7696611538c225f17d
Rashid Ayupov <113050035+Rashid-creator-droid@users.noreply.github.com>
2026-01-30 10:05:42 +0300

    Dev (#8)
    * result work
    
    * work result
    
    * error in readme

--- ml/trainer.py
+++ ml/trainer.py
@@ -1,9 +1,7 @@
 import json
-import random
 from typing import List
 
 import matplotlib.pyplot as plt
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -56,8 +54,6 @@ class Trainer:
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-        np.random.seed(seed)
-        random.seed(seed)
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
