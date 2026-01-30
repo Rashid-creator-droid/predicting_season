82ebaf0229cdbca513abf7dc3f97be2981fd27a8
Rashid <valentaine1234@gmail.com>
2026-01-30 04:08:50 +0300

    removed unnecessary models

--- ml/models.py
+++ ml/models.py
@@ -10,10 +10,7 @@ def get_model(model_name: str = "resnet18", num_classes: int = 4, pretrained: bo
     if model_name == "resnet18":
         model = models.resnet18(pretrained=pretrained)
         model.fc = nn.Linear(model.fc.in_features, num_classes)
-    elif model_name == "mobilenet_v3":
-        model = models.mobilenet_v3_small(pretrained=pretrained)
-        model.fc = torch.nn.Linear(1024, num_classes)
     else:
-        raise ValueError(f"nknown model name: {model_name}")
+        raise ValueError(f"Unknown model name: {model_name}")
 
     return model
